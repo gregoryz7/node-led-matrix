b673ab53bc2a43a9ff11857daef0a2cd7c5953ea
gregoryz7 <zackgreg7@gmail.com>
2016-11-21 03:21:00 +0000

    initial

---
+++ build/Release/.deps/Release/hzeller-matrix.node.d
@@ -0,0 +1,1 @@
+cmd_Release/hzeller-matrix.node := rm -rf "Release/hzeller-matrix.node" && cp -af "Release/obj.target/hzeller-matrix.node" "Release/hzeller-matrix.node"
---
+++ build/Release/.deps/Release/obj.target/hzeller-matrix/src/run-image.o.d
@@ -0,0 +1,218 @@
+cmd_Release/obj.target/hzeller-matrix/src/run-image.o := g++ '-DNODE_GYP_MODULE_NAME=hzeller-matrix' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/6.9.1/include/node -I/home/pi/.node-gyp/6.9.1/src -I/home/pi/.node-gyp/6.9.1/deps/uv/include -I/home/pi/.node-gyp/6.9.1/deps/v8/include -I/usr/include/GraphicsMagick -I../hzeller/include -I../../nan  -fPIC -pthread -Wall -Wall -O3 -g -O3 -std=gnu++0x -Wall -g -fno-strict-aliasing -O2 -pthread -I/usr/include/GraphicsMagick -MMD -MF ./Release/.deps/Release/obj.target/hzeller-matrix/src/run-image.o.d.raw   -c -o Release/obj.target/hzeller-matrix/src/run-image.o ../src/run-image.cpp
+Release/obj.target/hzeller-matrix/src/run-image.o: ../src/run-image.cpp \
+ ../src/addon.h ../src/includes.h ../../nan/nan.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node_version.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv-errno.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv-version.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv-unix.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv-threadpool.h \
+ /home/pi/.node-gyp/6.9.1/include/node/uv-linux.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node.h \
+ /home/pi/.node-gyp/6.9.1/include/node/v8.h \
+ /home/pi/.node-gyp/6.9.1/include/node/v8-version.h \
+ /home/pi/.node-gyp/6.9.1/include/node/v8config.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node_version.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node_buffer.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node.h \
+ /home/pi/.node-gyp/6.9.1/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h \
+ ../../nan/nan_typedarray_contents.h \
+ /usr/include/GraphicsMagick/Magick++.h \
+ /usr/include/GraphicsMagick/Magick++/Include.h \
+ /usr/include/GraphicsMagick/magick/magick_config.h \
+ /usr/include/GraphicsMagick/magick/api.h \
+ /usr/include/GraphicsMagick/magick/common.h \
+ /usr/include/GraphicsMagick/magick/magick_types.h \
+ /usr/include/GraphicsMagick/magick/analyze.h \
+ /usr/include/GraphicsMagick/magick/image.h \
+ /usr/include/GraphicsMagick/magick/forward.h \
+ /usr/include/GraphicsMagick/magick/colorspace.h \
+ /usr/include/GraphicsMagick/magick/error.h \
+ /usr/include/GraphicsMagick/magick/log.h \
+ /usr/include/GraphicsMagick/magick/timer.h \
+ /usr/include/GraphicsMagick/magick/attribute.h \
+ /usr/include/GraphicsMagick/magick/average.h \
+ /usr/include/GraphicsMagick/magick/blob.h \
+ /usr/include/GraphicsMagick/magick/cdl.h \
+ /usr/include/GraphicsMagick/magick/channel.h \
+ /usr/include/GraphicsMagick/magick/color.h \
+ /usr/include/GraphicsMagick/magick/color_lookup.h \
+ /usr/include/GraphicsMagick/magick/colormap.h \
+ /usr/include/GraphicsMagick/magick/command.h \
+ /usr/include/GraphicsMagick/magick/compare.h \
+ /usr/include/GraphicsMagick/magick/composite.h \
+ /usr/include/GraphicsMagick/magick/compress.h \
+ /usr/include/GraphicsMagick/magick/confirm_access.h \
+ /usr/include/GraphicsMagick/magick/constitute.h \
+ /usr/include/GraphicsMagick/magick/decorate.h \
+ /usr/include/GraphicsMagick/magick/delegate.h \
+ /usr/include/GraphicsMagick/magick/deprecate.h \
+ /usr/include/GraphicsMagick/magick/describe.h \
+ /usr/include/GraphicsMagick/magick/draw.h \
+ /usr/include/GraphicsMagick/magick/render.h \
+ /usr/include/GraphicsMagick/magick/type.h \
+ /usr/include/GraphicsMagick/magick/effect.h \
+ /usr/include/GraphicsMagick/magick/enhance.h \
+ /usr/include/GraphicsMagick/magick/fx.h \
+ /usr/include/GraphicsMagick/magick/gem.h \
+ /usr/include/GraphicsMagick/magick/random.h \
+ /usr/include/GraphicsMagick/magick/gradient.h \
+ /usr/include/GraphicsMagick/magick/hclut.h \
+ /usr/include/GraphicsMagick/magick/list.h \
+ /usr/include/GraphicsMagick/magick/magic.h \
+ /usr/include/GraphicsMagick/magick/magick.h \
+ /usr/include/GraphicsMagick/magick/memory.h \
+ /usr/include/GraphicsMagick/magick/module.h \
+ /usr/include/GraphicsMagick/magick/monitor.h \
+ /usr/include/GraphicsMagick/magick/montage.h \
+ /usr/include/GraphicsMagick/magick/operator.h \
+ /usr/include/GraphicsMagick/magick/paint.h \
+ /usr/include/GraphicsMagick/magick/pixel_cache.h \
+ /usr/include/GraphicsMagick/magick/pixel_iterator.h \
+ /usr/include/GraphicsMagick/magick/plasma.h \
+ /usr/include/GraphicsMagick/magick/profile.h \
+ /usr/include/GraphicsMagick/magick/quantize.h \
+ /usr/include/GraphicsMagick/magick/registry.h \
+ /usr/include/GraphicsMagick/magick/resize.h \
+ /usr/include/GraphicsMagick/magick/resource.h \
+ /usr/include/GraphicsMagick/magick/shear.h \
+ /usr/include/GraphicsMagick/magick/signature.h \
+ /usr/include/GraphicsMagick/magick/statistics.h \
+ /usr/include/GraphicsMagick/magick/texture.h \
+ /usr/include/GraphicsMagick/magick/transform.h \
+ /usr/include/GraphicsMagick/magick/utility.h \
+ /usr/include/GraphicsMagick/magick/version.h \
+ /usr/include/GraphicsMagick/Magick++/Image.h \
+ /usr/include/GraphicsMagick/Magick++/Blob.h \
+ /usr/include/GraphicsMagick/Magick++/Color.h \
+ /usr/include/GraphicsMagick/Magick++/Drawable.h \
+ /usr/include/GraphicsMagick/Magick++/Geometry.h \
+ /usr/include/GraphicsMagick/Magick++/Exception.h \
+ /usr/include/GraphicsMagick/Magick++/TypeMetric.h \
+ /usr/include/GraphicsMagick/Magick++/Pixels.h \
+ /usr/include/GraphicsMagick/Magick++/STL.h \
+ /usr/include/GraphicsMagick/Magick++/CoderInfo.h \
+ /usr/include/GraphicsMagick/Magick++/Montage.h \
+ ../hzeller/include/led-matrix.h ../hzeller/include/gpio.h \
+ ../hzeller/include/canvas.h ../hzeller/include/thread.h \
+ ../hzeller/include/transformer.h ../src/animation.h ../src/matrix.h \
+ ../src/image-animation.h
+../src/run-image.cpp:
+../src/addon.h:
+../src/includes.h:
+../../nan/nan.h:
+/home/pi/.node-gyp/6.9.1/include/node/node_version.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv-errno.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv-version.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv-unix.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv-threadpool.h:
+/home/pi/.node-gyp/6.9.1/include/node/uv-linux.h:
+/home/pi/.node-gyp/6.9.1/include/node/node.h:
+/home/pi/.node-gyp/6.9.1/include/node/v8.h:
+/home/pi/.node-gyp/6.9.1/include/node/v8-version.h:
+/home/pi/.node-gyp/6.9.1/include/node/v8config.h:
+/home/pi/.node-gyp/6.9.1/include/node/node_version.h:
+/home/pi/.node-gyp/6.9.1/include/node/node_buffer.h:
+/home/pi/.node-gyp/6.9.1/include/node/node.h:
+/home/pi/.node-gyp/6.9.1/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_typedarray_contents.h:
+/usr/include/GraphicsMagick/Magick++.h:
+/usr/include/GraphicsMagick/Magick++/Include.h:
+/usr/include/GraphicsMagick/magick/magick_config.h:
+/usr/include/GraphicsMagick/magick/api.h:
+/usr/include/GraphicsMagick/magick/common.h:
+/usr/include/GraphicsMagick/magick/magick_types.h:
+/usr/include/GraphicsMagick/magick/analyze.h:
+/usr/include/GraphicsMagick/magick/image.h:
+/usr/include/GraphicsMagick/magick/forward.h:
+/usr/include/GraphicsMagick/magick/colorspace.h:
+/usr/include/GraphicsMagick/magick/error.h:
+/usr/include/GraphicsMagick/magick/log.h:
+/usr/include/GraphicsMagick/magick/timer.h:
+/usr/include/GraphicsMagick/magick/attribute.h:
+/usr/include/GraphicsMagick/magick/average.h:
+/usr/include/GraphicsMagick/magick/blob.h:
+/usr/include/GraphicsMagick/magick/cdl.h:
+/usr/include/GraphicsMagick/magick/channel.h:
+/usr/include/GraphicsMagick/magick/color.h:
+/usr/include/GraphicsMagick/magick/color_lookup.h:
+/usr/include/GraphicsMagick/magick/colormap.h:
+/usr/include/GraphicsMagick/magick/command.h:
+/usr/include/GraphicsMagick/magick/compare.h:
+/usr/include/GraphicsMagick/magick/composite.h:
+/usr/include/GraphicsMagick/magick/compress.h:
+/usr/include/GraphicsMagick/magick/confirm_access.h:
+/usr/include/GraphicsMagick/magick/constitute.h:
+/usr/include/GraphicsMagick/magick/decorate.h:
+/usr/include/GraphicsMagick/magick/delegate.h:
+/usr/include/GraphicsMagick/magick/deprecate.h:
+/usr/include/GraphicsMagick/magick/describe.h:
+/usr/include/GraphicsMagick/magick/draw.h:
+/usr/include/GraphicsMagick/magick/render.h:
+/usr/include/GraphicsMagick/magick/type.h:
+/usr/include/GraphicsMagick/magick/effect.h:
+/usr/include/GraphicsMagick/magick/enhance.h:
+/usr/include/GraphicsMagick/magick/fx.h:
+/usr/include/GraphicsMagick/magick/gem.h:
+/usr/include/GraphicsMagick/magick/random.h:
+/usr/include/GraphicsMagick/magick/gradient.h:
+/usr/include/GraphicsMagick/magick/hclut.h:
+/usr/include/GraphicsMagick/magick/list.h:
+/usr/include/GraphicsMagick/magick/magic.h:
+/usr/include/GraphicsMagick/magick/magick.h:
+/usr/include/GraphicsMagick/magick/memory.h:
+/usr/include/GraphicsMagick/magick/module.h:
+/usr/include/GraphicsMagick/magick/monitor.h:
+/usr/include/GraphicsMagick/magick/montage.h:
+/usr/include/GraphicsMagick/magick/operator.h:
+/usr/include/GraphicsMagick/magick/paint.h:
+/usr/include/GraphicsMagick/magick/pixel_cache.h:
+/usr/include/GraphicsMagick/magick/pixel_iterator.h:
+/usr/include/GraphicsMagick/magick/plasma.h:
+/usr/include/GraphicsMagick/magick/profile.h:
+/usr/include/GraphicsMagick/magick/quantize.h:
+/usr/include/GraphicsMagick/magick/registry.h:
+/usr/include/GraphicsMagick/magick/resize.h:
+/usr/include/GraphicsMagick/magick/resource.h:
+/usr/include/GraphicsMagick/magick/shear.h:
+/usr/include/GraphicsMagick/magick/signature.h:
+/usr/include/GraphicsMagick/magick/statistics.h:
+/usr/include/GraphicsMagick/magick/texture.h:
+/usr/include/GraphicsMagick/magick/transform.h:
+/usr/include/GraphicsMagick/magick/utility.h:
+/usr/include/GraphicsMagick/magick/version.h:
+/usr/include/GraphicsMagick/Magick++/Image.h:
+/usr/include/GraphicsMagick/Magick++/Blob.h:
+/usr/include/GraphicsMagick/Magick++/Color.h:
+/usr/include/GraphicsMagick/Magick++/Drawable.h:
+/usr/include/GraphicsMagick/Magick++/Geometry.h:
+/usr/include/GraphicsMagick/Magick++/Exception.h:
+/usr/include/GraphicsMagick/Magick++/TypeMetric.h:
+/usr/include/GraphicsMagick/Magick++/Pixels.h:
+/usr/include/GraphicsMagick/Magick++/STL.h:
+/usr/include/GraphicsMagick/Magick++/CoderInfo.h:
+/usr/include/GraphicsMagick/Magick++/Montage.h:
+../hzeller/include/led-matrix.h:
+../hzeller/include/gpio.h:
+../hzeller/include/canvas.h:
+../hzeller/include/thread.h:
+../hzeller/include/transformer.h:
+../src/animation.h:
+../src/matrix.h:
+../src/image-animation.h:
